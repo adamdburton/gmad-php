8b248b650f71c5c41eb9d12088a96c5a3c0a324b
Adam Burton <adam@burt0n.net>
2015-05-19 09:03:46 +0100

    Small fixes

--- src/AddonWriter.php
+++ src/AddonWriter.php
@@ -13,15 +13,17 @@ class AddonWriter
     
     public function write($folder, $files, $title, $description)
     {
+        $folder = rtrim($folder, '/') . '/';
+
         $int8 = 'c'; // signed char (8)
         $int32 = 'i'; // signed integer (32)
         $uint32 = 'I'; // unsigned integer (32)
-        $int64 = 'q'; // signed long long (64)
+        $uint64 = 'Q'; // unsigned long long (64)
 
         $this->writeString(Addon::ident, false); // Ident (4)
         $this->writeInt(Addon::version, $int8); // Version (1)
-        $this->writeInt(0, $int64); // SteamID (64) [unused]
-        $this->writeInt(time(), $int64); // TimeStamp (64)
+        $this->writeInt(0, $uint64); // SteamID (64) [unused]
+        $this->writeInt(time(), $uint64); // TimeStamp (64)
         $this->writeInt(0, $int8); // Required content, ignored
 
         $this->writeString($title); // Addon Name (n)
@@ -36,6 +38,8 @@ class AddonWriter
 
         foreach($files as $file)
         {
+            $file = ltrim($file, '/');
+
             $crc = crc32($folder . $file);
             $size = filesize($folder . $file);
 
@@ -43,7 +47,7 @@ class AddonWriter
 
             $this->writeInt($fileNumber, $uint32); // File number (32)
             $this->writeString(strtolower($file)); // File name (lowercase) (n)
-            $this->writeInt($size, $int64); // File size (64)
+            $this->writeInt($size, $uint64); // File size (64)
             $this->writeInt($crc, $uint32); // File crc (32)
         }
 
@@ -51,6 +55,8 @@ class AddonWriter
 
         foreach($files as $file)
         {
+            $file = ltrim($file, '/');
+
             $contents = file_get_contents($folder . $file);
 
             $this->writeString($contents);
